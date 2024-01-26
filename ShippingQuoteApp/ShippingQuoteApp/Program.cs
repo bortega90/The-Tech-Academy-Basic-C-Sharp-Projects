27e8215e288d0f5fd35cc537894fff9b2f6d5c55
bortega90 <britnyortega@gmail.com>
2024-01-26 12:58:55 -0800

    fixed code
    code stops when requirements aren't met

--- ShippingQuoteApp/ShippingQuoteApp/Program.cs
+++ ShippingQuoteApp/ShippingQuoteApp/Program.cs
@@ -19,40 +19,36 @@ namespace ShippingQuoteApp
             {
                 Console.WriteLine("Package is too heavy to ship via Package Express. Have a good day.");
             }
-            else if (weight <= 50) 
+            else if (weight <= 50)
             {
                 Console.WriteLine("We can help");
-            }
-            else 
-            {
-                Console.WriteLine("error");
-            }
-            //input package dimensions
-            Console.WriteLine("What is your package width?");
-            int width = Convert.ToInt32(Console.ReadLine()); 
-            Console.WriteLine("What is your package height?");
-            int height = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is your package length?");
-            int length = Convert.ToInt32(Console.ReadLine());
-            int dimensions = width + height + length;
-            //package dimensions limit is 50
-            
-            if (dimensions > 50)
-            {
-                Console.WriteLine("Your package is too big, sorry");
-            }
-            else if (dimensions < 50)
-            {
-                Console.WriteLine("Your quote is coming up");
-            }
-            else 
-            {
-                Console.WriteLine("error");
-            }
-            decimal quote = (width * height * length * weight)/100.00m ;
-            Console.WriteLine("Your quote is " + quote);
+                //input package dimensions
+                Console.WriteLine("What is your package width?");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is your package height?");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is your package length?");
+                int length = Convert.ToInt32(Console.ReadLine());
+                int dimensions = width + height + length;
+                //package dimensions limit is 50
+                if (dimensions > 50)
+                {
+                    Console.WriteLine("Your package is too big, sorry");
+                }
+                else
+                {
+                    if (dimensions <= 50)
+                    {
+                        Console.WriteLine("Your quote is on the way");
+                           //calculating quote
+                        decimal quote = (width * height * length * weight) / 100.00m;
+                        Console.WriteLine("Your quote is " + quote);
+                    }
+                }
+                Console.ReadLine();
 
-            Console.ReadLine();
+
+            }
         }
     }
 }
