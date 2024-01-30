c5f0196328257835145f7692f035bbba232a4936
bortega90 <britnyortega@gmail.com>
2024-01-30 14:26:03 -0800

    push StringAndInteger

---
+++ StringAndIntegers/StringAndIntegers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int> { 5, 10, 15, 20, 25 };
+                Console.WriteLine("Pick a number.");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    Console.WriteLine("Dividing the pick picked number to the numbers in the list");
+                    int divideBy = numbers[i] / userNumber;
+                    Console.WriteLine(numbers[i] + " divide by " + userNumber + " equals " + divideBy);
+
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
