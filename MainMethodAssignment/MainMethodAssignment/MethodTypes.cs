bfce2179de1d35b279b0d361569592fabf6d38b3
bortega90 <britnyortega@gmail.com>
2024-01-31 15:30:36 -0800

    Main Method Assignment
    I made a class with three methods with all the same name that returned different types of data. I made an integer method, decimal method and a string method

---
+++ MainMethodAssignment/MainMethodAssignment/MethodTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MethodTypes
+    {
+        //integer method
+        public int Methods(int x) { return x + 5; }
+        //decimal method
+        public decimal Methods(decimal x) { return x/3; }
+        //string method
+        public string Methods(string x) { return x; }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //class and instantiate to methods in class
+            MethodTypes obj = new MethodTypes();
+            int number = Convert.ToInt32(6);
+            //calling addition method with instance and variable number
+            Console.WriteLine(obj.Methods(number));
+            Console.ReadLine();
+
+            //class and instantiate the second method in class
+            MethodTypes obj2 = new MethodTypes();
+            decimal number2 = Convert.ToDecimal(3.75);
+            //calling division method with instance and variable number2
+            Console.WriteLine(obj2.Methods(number2));
+            Console.ReadLine();
+
+            //class and instantiate the third method in class
+            MethodTypes obj3 = new MethodTypes();
+            string word = "Britny";
+            //Calling string method with instance and variable word
+            Console.WriteLine(obj3.Methods(word));
+            Console.ReadLine();
+
+        }
+        
+    }
+    
+
+}
