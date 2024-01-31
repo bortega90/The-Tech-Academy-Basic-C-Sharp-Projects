63efddc57f5d766ca32d23940f753ed91692968f
bortega90 <britnyortega@gmail.com>
2024-01-31 14:21:31 -0800

    Calling Methods Assignment
    I created a class with three methods (thx frank!) and was able to call all three methods on the the main() program

---
+++ CallingMethods/CallingMethods/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CallingMethods
+{
+    public class Math
+    {
+
+        //method 1/3 addition
+        public int Addition(int x) { return x + 5; }
+        //method 2/3 subtraction
+        public int Subtraction(int x) {  return x - 5; }
+        //method 3/3 multiplication
+        public int Multiplication(int x) { return x * 5; }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //class,instance to call method
+            Math obj = new Math();
+            //asking user to input number
+            Console.WriteLine("\nPlease pick a number:\t");
+            //saving input as userInput
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            //calling addition method with instance and adding userInput
+            Console.WriteLine(obj.Addition(userInput));
+            //calling subtraction method ''  ''
+            Console.WriteLine(obj.Subtraction(userInput));
+            //calling multiplication method '' ''
+            Console.WriteLine(obj.Multiplication(userInput));
+            Console.ReadLine();
+        }
+        
+    }
+}
