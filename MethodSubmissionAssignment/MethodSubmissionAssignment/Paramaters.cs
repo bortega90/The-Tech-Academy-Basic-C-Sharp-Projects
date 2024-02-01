4767098979b38faf296b75bc28ce20371b939787
bortega90 <britnyortega@gmail.com>
2024-01-31 17:45:24 -0800

    need help
    I think  I set up an optional input, but do not know how to skip input on the console to continue the code

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Paramaters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    public class Paramaters
+    {
+        public int Parameters(int x, int y = 2) { return x + y; }
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //class and instantiate to methods in class 
+            Paramaters obj = new Paramaters();
+            //ask user to input two numbers, the second one is optional
+            Console.WriteLine("Input two numbers.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number is optional");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            //calling method with instance and variables 
+            Console.WriteLine(obj.Parameters(num1, num2));
+            Console.ReadLine();
+        }
+    }
+}
