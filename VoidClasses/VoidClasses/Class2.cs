ee94afb24b0d464cd0929442d30e6a80c70a53e5
bortega90 <britnyortega@gmail.com>
2024-02-01 17:06:01 -0800

    push

--- VoidClasses/VoidClasses/Class2.cs
+++ VoidClasses/VoidClasses/Class2.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VoidClasses
 {
-    static class Class2
+    public class Class2
     {
-        static string Method(string b)//confused on ouput parameters
+        //create static method() with out parameters
+        public static void Method(out string b)
         {
-            b = "Hello";
+            b = " ";
             Console.WriteLine(b);
         }
     }
 }
+
--- VoidClasses/VoidClasses/Program.cs
+++ VoidClasses/VoidClasses/Program.cs
@@ -9,16 +9,20 @@ namespace VoidClasses
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
             //class and instantiate
             Class1 class1 = new Class1();
             Class2 class2 = new Class2();
-            //Class2 class2 = new Class2();
             //ask user to input an integer
             Console.WriteLine("Please input a number");
             int input = Convert.ToInt32(Console.ReadLine());
+            //ask user to type a string
+            Console.WriteLine("Please type a word");
+            string word = Console.ReadLine();
+
+            //calling overload Method() from both classes
             class1.Method(input);
-            class2.Method();
+            class2.Method(b:out word) ;
             
             Console.ReadLine();
         }
