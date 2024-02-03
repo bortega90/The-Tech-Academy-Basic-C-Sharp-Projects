97931598bedf7b5ba361ee2dec306c8e316e6388
bortega90 <britnyortega@gmail.com>
2024-02-02 17:13:20 -0800

    push

---
+++ ParsingEnums/ParsingEnums/DaysOfWeek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public class DaysOfWeek
+    {
+        public DayOfWeekType DayOfWeek { get; set; }
+    }
+    public enum DayOfWeekType
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //ask user to enter current day
+                Console.WriteLine("What day is today?");
+                //dayofweektype is data type
+                DayOfWeekType input = Console.ReadLine();
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Please type day of the week.")
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
