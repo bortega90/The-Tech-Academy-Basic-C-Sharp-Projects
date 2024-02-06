55f69e0edcd8b469f924c66048cb1f4f89a40dd2
bortega90 <britnyortega@gmail.com>
2024-02-06 13:56:45 -0800

    updated comments

--- DateTime/DateTime/Program.cs
+++ DateTime/DateTime/Program.cs
@@ -10,11 +10,15 @@ namespace DateTimeAssignment
     {
         static void Main(string[] args)
         {
-            
+            //displaying time in console
             Console.WriteLine("Today's time is {0}", DateTime.Now);
+            //aksing user for an integer
             Console.WriteLine("Please type an integer.");
+            //saving user's input as variable answer
             int answer = Convert.ToInt32(Console.ReadLine());
+            //calculating new time in hours with user's integer
             DateTime newTime = DateTime.Now.AddHours(answer);
+            //displaying time in x hrs ahead
             Console.WriteLine("The time in {0} hours will be {1}", answer, newTime);
             Console.ReadLine();
         }
