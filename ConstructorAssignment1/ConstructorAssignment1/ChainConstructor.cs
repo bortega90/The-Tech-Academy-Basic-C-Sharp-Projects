504f39dfcf07ac5ec94a8eab34dc93a8c66c3ca1
bortega90 <britnyortega@gmail.com>
2024-02-06 18:11:03 -0800

    push

---
+++ ConstructorAssignment1/ConstructorAssignment1/ChainConstructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment1
+{
+    public class ChainConstructor
+    {
+        //setting properties for class
+        private int id;
+        private string name;
+
+        //constructor              //assigning id to 001 and name to bob
+        public ChainConstructor() : this (001, "Bob")
+        {
+
+        }
+        // calling constructor passing two parameters
+        public ChainConstructor(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }   
+    
+}
---
+++ ConstructorAssignment1/ConstructorAssignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constant variable
+            const string fName = "Britny";
+            // var variable
+            var lName = "Ortega";
+            Console.WriteLine("My name is {0} {1}.",  fName, lName);
+            Console.ReadLine();
+        }
+    }
+    
+}
