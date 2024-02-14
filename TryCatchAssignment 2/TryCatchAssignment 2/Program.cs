6648a0ff30938e315dffc25996a68228bf5cc83f
bortega90 <britnyortega@gmail.com>
2024-02-13 17:51:38 -0800

    Newsletter

---
+++ TryCatchAssignment 2/TryCatchAssignment 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            DateTime now = new DateTime(2024);
+            
+            TimeSpan year = now - age;
+        }
+    }
+}
