ce0af1c2a248ea45001e0fd3af68aab52a98a2cf
bortega90 <britnyortega@gmail.com>
2024-02-14 18:11:39 -0800

    push
    fixed calculations, made edits to create and edit pages and added admin view.

--- CarInsurance(MVC)/CarInsurance(MVC)/Controllers/InsureeController.cs
+++ CarInsurance(MVC)/CarInsurance(MVC)/Controllers/InsureeController.cs
@@ -15,10 +15,13 @@ namespace CarInsurance_MVC_.Controllers
     {
         private InsuranceEntities db = new InsuranceEntities();
 
-        public ActionResult Quote()
+        public decimal CalcQuote(Insuree insuree)
         {
+            //base amount for insurance
             decimal baseCost = 50.00m;
-            int age = Convert.ToInt32(Console.ReadLine());
+            //calculating insuree's age
+            int age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+            //calculating age requirements
             if (age <= 18)
             {
                 baseCost += 100m;
@@ -32,21 +35,44 @@ namespace CarInsurance_MVC_.Controllers
                 baseCost += 25m;
             }
             
-            int carYear = Convert.ToInt32(Console.ReadLine());
-            if (carYear < 2000 && carYear > 2015)
+            //calculating car requirements 
+            if (insuree.CarYear < 2000 || insuree.CarYear > 2015)
             {
                 baseCost += 25m;
             }
-            string carMake = Console.ReadLine();
-            if (carMake == "Porche")
+            
+            if (insuree.CarMake == "Porsche")
             {
                 baseCost += 25m;
             }
-            string carModel = Console.ReadLine();
-            if (carModel == "911 Carrera" && carMake == "Porche")
+
+            if (insuree.CarModel == "911 Carrera" && insuree.CarMake == "Porsche")
             {
                 baseCost += 25m;
             }
+            
+            //calculating insuree's ticket history
+            if (insuree.SpeedingTickets > 0)
+            {
+                baseCost += 25 * insuree.SpeedingTickets;
+            }
+            if (insuree.DUI == true)
+            {
+                baseCost += baseCost * 0.25m; 
+            }
+            
+            //calculating insuree's full coverage cost
+            if (insuree.CoverageType == true)
+            {
+                baseCost += baseCost * 0.50m;
+            }
+            return baseCost;
+
+        }
+
+        public ActionResult Admin()
+        {
+            return View(db.Insurees.ToList());
         }
 
         // GET: Insuree
@@ -85,6 +111,8 @@ namespace CarInsurance_MVC_.Controllers
         {
             if (ModelState.IsValid)
             {
+                //this will create new insuree
+                insuree.Quote = CalcQuote(insuree);
                 db.Insurees.Add(insuree);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -105,6 +133,7 @@ namespace CarInsurance_MVC_.Controllers
             {
                 return HttpNotFound();
             }
+
             return View(insuree);
         }
 
@@ -118,6 +147,8 @@ namespace CarInsurance_MVC_.Controllers
             if (ModelState.IsValid)
             {
                 db.Entry(insuree).State = EntityState.Modified;
+                //added to post edits
+                insuree.Quote = CalcQuote(insuree);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
